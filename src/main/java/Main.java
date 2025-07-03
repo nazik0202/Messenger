708b8af7680dec8ea3df602910e9f761b4893767
NazikTazik123 <Nazariy.ya@gmail.com>
2025-07-03 13:13:03 +0300

    add data to gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         FileChatStorage fileChatStorage = new FileChatStorage( "C:\\Users\\nazar\\IdeaProjects\\messenger\\data\\Chats");
-        Chat coolChat = new Chat(1,"coolChat");
-//        fileChatStorage.saveChat(coolChat);
+        Chat coolChat = new Chat(0,"cool_Chat");
+        fileChatStorage.saveChat(coolChat);
         System.out.println(fileChatStorage.loadMessages(coolChat));
 
     }
